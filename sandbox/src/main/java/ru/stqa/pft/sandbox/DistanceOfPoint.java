986e4d3ceb09cc79955e275291b3c7bfc5038658
Olgaidukova <olya.gaidukova@gmail.com>
2022-08-19 16:20:45 +0300

    Update p.4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceOfPoint.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceOfPoint.java
@@ -8,9 +8,8 @@ public class DistanceOfPoint {
     System.out.println("Coordinates of the point p1: (" + p1.x + ";" + p1.y + ")");
     Point p2 = new Point(10,10);
     System.out.println("Coordinates of the point p2: (" + p2.x + ";" + p2.y + ")");
-    System.out.println("Distance between two points = " + distance(p1, p2));
+
+    System.out.println("Distance between two points = " + p1.distance(p2));
 }
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)));
-  }
+
 }
