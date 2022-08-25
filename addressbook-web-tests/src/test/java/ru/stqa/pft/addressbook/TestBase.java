010818cf48ac34279922f8b9d84e22fc9c630da4
Olgaidukova <olya.gaidukova@gmail.com>
2022-08-25 19:31:42 +0300

    Task 5. TestBase for Contact

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -21,6 +21,40 @@ public class TestBase {
         login("admin", "secret");
     }
 
+    public void goToHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+      wd.get("http://localhost:8080/addressbook/index.php");
+    }
+
+    public void submitContactForm() {
+      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    public void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).click();
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.firstname());
+      wd.findElement(By.name("lastname")).click();
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.lastname());
+      wd.findElement(By.name("address")).click();
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.address());
+      wd.findElement(By.name("email")).click();
+      wd.findElement(By.name("email")).clear();
+      wd.findElement(By.name("email")).sendKeys(contactData.email());
+      wd.findElement(By.name("theform")).click();
+      wd.findElement(By.name("home")).click();
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.phone());
+      wd.findElement(By.name("theform")).click();
+    }
+
+    public void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
+      wd.get("http://localhost:8080/addressbook/edit.php");
+    }
+
     private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
