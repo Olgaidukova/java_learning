ad346ae8a2e0cc4476437a356968f3f8ad0d8a4a
Olgaidukova <olya.gaidukova@gmail.com>
2022-08-26 17:28:42 +0300

    Task 5. Creation HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -4,36 +4,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactHelper {
-   private WebDriver wd;
+public class ContactHelper  extends HelperBase{
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void submitContactForm() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.firstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.lastname());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.address());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.email());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.phone());
+        type(By.name("firstname"),contactData.firstname());
+        type(By.name("lastname"),contactData.lastname());
+        type(By.name("address"),contactData.address());
+        type(By.name("email"),contactData.email());
+        type(By.name("home"),contactData.phone());
     }
 
     public void initContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
       wd.get("http://localhost:8080/addressbook/edit.php");
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,19 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-   private WebDriver wd;
-
+public class NavigationHelper extends HelperBase {
     public NavigationHelper(WebDriver wd) {
-      this.wd = wd;
+        super(wd);
     }
 
     public void goToHomePage() {
-      wd.findElement(By.linkText("home page")).click();
+        click(By.linkText("home page"));
       wd.get("http://localhost:8080/addressbook/index.php");
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
