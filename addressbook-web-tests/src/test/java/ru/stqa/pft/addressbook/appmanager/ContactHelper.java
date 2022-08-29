7b1e1e732097e28caef0c2a4b64a9b9883c01f52
Olgaidukova <olya.gaidukova@gmail.com>
2022-08-29 16:16:50 +0300

    Task 6. Creation ContactModificationTest

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -25,4 +25,12 @@ public class ContactHelper  extends HelperBase{
         click(By.linkText("add new"));
       wd.get("http://localhost:8080/addressbook/edit.php");
     }
+
+    public void initContactModification() {
+        click(By.xpath("//img[@alt='Edit']"));
+    }
+
+    public void submitContactModification() {
+        click(By.xpath("//div[@id='content']/form/input[22]"));
+    }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTest extends TestBase{
+    @Test
+    public void testContactModification() {
+        app.getNavigationHelper().gotoContactPage();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("Test1", "Testov1", "SPB1", "test1@test.com", "1234561"));
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().goToHomePage();
+    }
+}
