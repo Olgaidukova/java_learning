3476c7aedff2a3ad7d53f6a3ab23078713d56701
Olgaidukova <olya.gaidukova@gmail.com>
2022-09-09 15:13:54 +0300

    Task 8. Comparison of collections

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,4 +1,27 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public record ContactData(String firstname, String lastname, String address, String email, String phone) {
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstname='" + firstname + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        return Objects.equals(firstname, that.firstname);
+    }
+
+    @Override
+    public int hashCode() {
+        return firstname != null ? firstname.hashCode() : 0;
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,4 +1,27 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public record GroupData(String name, String header, String footer) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
