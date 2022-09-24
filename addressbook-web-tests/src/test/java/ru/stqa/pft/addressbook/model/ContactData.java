9b1612afdd3018c40727222e5be3eebd9b7aeb77
Olgaidukova <olya.gaidukova@gmail.com>
2022-09-24 14:37:21 +0300

    5.11. gluing

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -12,6 +12,17 @@ public class ContactData {
     private String home;
     private String work;
     private String mobile;
+    private String allPhones;
+
+    public String getAllPhones() {
+        return allPhones;
+    }
+
+    public ContactData withAllPhones(String allPhones) {
+        this.allPhones = allPhones;
+        return this;
+    }
+
 
 
     public int getId() {
